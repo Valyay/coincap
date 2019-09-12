b5307ebe187f10fc2700390de44e23dc01956151
Евгений Валяев <valyay@MacBook-Pro-Evgenij.local>
2019-09-13 00:25:01 +0300

    Add API request

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,30 @@ import React, { Component } from "react";
 import "../styles/App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataCoins: {}
+    };
+  }
+
+  componentDidMount() {
+    const options = {
+      method: "GET",
+      hostname: "api.coincap.io",
+      path: "/v2/assets",
+      headers: {}
+    };
+
+    fetch("https://api.coincap.io/v2/assets", options)
+      .then(response => response.json())
+      .then(({ data }) => {
+        this.setState({ dataCoins: data });
+        return data;
+      })
+      .catch(error => console.error("Error:", error));
+  }
+
   render() {
     return (
       <div>

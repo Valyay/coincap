baa1255acacf63c1c3022f1dcbe7807b2f0c89d2
Evgeniy Valyaev <evgesha.valyaev@gmail.com>
2019-10-09 11:50:40 +0300

    Add bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -10,8 +10,8 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <h1>My React App!</h1>
+      <div className="container">
+        MyApp
         <Table />
       </div>
     );
